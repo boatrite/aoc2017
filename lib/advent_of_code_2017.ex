3fbba17eb3e84c7a214046ba61a11903448b8451
Gwen Boatrite <gwen.boatrite@gmail.com>
2017-12-05 23:45:39 -0600

    day 3 part 1

--- lib/advent_of_code_2017.ex
+++ lib/advent_of_code_2017.ex
@@ -45,6 +45,23 @@ defmodule AdventOfCode2017 do
     end)
     |> Enum.sum
   end
+
+  # Brute force solution using Streams. I'm sure there's a constant time
+  # mathematical solution, but I can't think of it.
+  def day_3_pt_1(input) do
+    Stream.iterate(1, &(&1 + 2)) # 1, 3, 5, 7
+    |> Stream.map(&(&1 * &1)) # 1, 9, 25, 49
+    |> Stream.with_index # {1, 1}, {9, 2}, {25, 3}, {49, 4} The 2nd number is the box number.
+    |> Stream.filter(fn {box_max, _} -> input <= box_max end) # This tells us what box we're in.
+    |> Stream.map(fn {box_max, box_n} ->
+      axis_start = box_max - 7*box_n
+      0..3
+      |> Enum.map(fn i -> 2*box_n*i + axis_start end)
+      |> Enum.map(&(abs(input - &1) + box_n))
+      |> Enum.min
+    end)
+    |> Enum.at(0)
+  end
 end
 
 defmodule Combine do
