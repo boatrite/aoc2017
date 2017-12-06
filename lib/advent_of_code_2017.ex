903ab4a4befb9aa750e8812db83a37e25290b8cf
Gwen Boatrite <gwen.boatrite@gmail.com>
2017-12-05 20:49:12 -0600

    day 2 part 2

--- lib/advent_of_code_2017.ex
+++ lib/advent_of_code_2017.ex
@@ -28,4 +28,36 @@ defmodule AdventOfCode2017 do
     Enum.map(fn {min, max} -> max - min end) |>
     Enum.sum
   end
+
+  def day_2_pt_2(input) do
+    String.split(input, "\n")
+    |> Enum.map(&String.trim(&1))
+    |> Enum.map(&String.split(&1, ~r"\s"))
+    |> Enum.map(&Enum.map(&1, fn el -> String.to_integer(el) end))
+    |> Enum.map(&Combine.combine(&1))
+    |> Enum.flat_map(fn row_combinations ->
+      Enum.filter(row_combinations, fn pair ->
+        rem(Enum.at(pair, 1), Enum.at(pair, 0)) == 0
+      end)
+    end)
+    |> Enum.map(fn pair ->
+      Enum.at(pair, 1) / Enum.at(pair, 0)
+    end)
+    |> Enum.sum
+  end
+end
+
+defmodule Combine do
+  def combine(enumerable) do
+    Enum.map(enumerable, fn el ->
+      [el, Enum.reject(enumerable, fn el2 -> el == el2 end)]
+    end)
+    |> Enum.flat_map(fn array ->
+      head = Enum.at array, 0
+      tail = Enum.at array, 1
+      Enum.map(tail, fn el -> [head, el] end)
+    end)
+    |> Enum.map(&Enum.sort(&1))
+    |> Enum.uniq
+  end
 end
