380e69769415a73f5cfe01dabfe964beacd53d00
Gwen Boatrite <gwen.boatrite@gmail.com>
2017-12-09 09:16:29 -0600

    Day 3 Part 2 refactor

--- lib/day_3_part_2.ex
+++ lib/day_3_part_2.ex
@@ -4,9 +4,7 @@ defmodule Day3Part2 do
   end
 
   def spiral do
-    initial = [1, 1, 2, 4, 5, 10, 11, 23, 25, 26, 54, 57, 59, 122, 133, 142,
-               147, 304, 330, 351, 362]
-
+    initial = [1]
     Stream.iterate(0, &add_1/1)
     |> Stream.transform(initial, fn i, curr_spiral ->
       val = Enum.at(curr_spiral, i)
@@ -21,22 +19,31 @@ defmodule Day3Part2 do
   end
 
   def identify(given_box_number) do
-    Enum.map([oranges(), yellows(), cornerstone_greens(), follower_greens()], fn stream ->
+    Enum.map([violets(), blues(), oranges(), yellows(), greens(), magentas()], fn stream ->
       Stream.filter(stream, fn {box_number, _, _} ->
         box_number >= given_box_number
       end)
       |> Enum.at(0)
     end)
+    |> Enum.reject(&is_nil/1)
     |> Enum.filter(fn {box_number, _, _} ->
       box_number == given_box_number
     end)
     |> Enum.at(0)
   end
 
+  def indices({_, :violet, _}) do
+    [0]
+  end
+
+  def indices({box_number, :blue, _}) do
+    [0, box_number - 2, box_number - 1]
+  end
+
   def indices({box_number, :o, type_index}) do
     index_deltas = Stream.concat(
       [0],
-      Stream.iterate(1, &add_1/1)
+      Stream.iterate(0, &add_1/1)
       |> Stream.flat_map(&dupe/1)
     )
     first_index = 0
@@ -47,38 +54,38 @@ defmodule Day3Part2 do
 
   def indices({box_number, :y, type_index}) do
     index_deltas = Stream.concat(
-      [0],
+      [0, 1],
       Stream.iterate(2, &add_1/1)
       |> Stream.flat_map(&dupe/1)
     )
-    first_index = 1
+    first_index = 0
     cornerstone_index_stream = Stream.scan(index_deltas, first_index, &sum/2)
     cornerstone_index = Enum.at(cornerstone_index_stream, type_index)
     [cornerstone_index, cornerstone_index + 1, box_number - 1]
   end
 
-  def indices({box_number, :cg, type_index}) do
+  def indices({box_number, :green, type_index}) do
     index_deltas = Stream.concat(
       [0],
-      Stream.iterate(3, &add_1/1)
+      Stream.iterate(1, &add_1/1)
       |> Stream.flat_map(&dupe/1)
     )
-    first_index = 6
+    first_index = 0
     cornerstone_index_stream = Stream.scan(index_deltas, first_index, &sum/2)
     cornerstone_index = Enum.at(cornerstone_index_stream, type_index)
     [cornerstone_index, cornerstone_index + 1, box_number - 2, box_number - 1]
   end
 
-  def indices({box_number, :fg, type_index}) do
+  def indices({box_number, :magenta, type_index}) do
     index_deltas = Stream.concat(
       [0],
-      Stream.iterate(1, &add_1/1)
+      Stream.iterate(0, &add_1/1)
       |> Stream.flat_map(&dupe/1)
       |> Stream.flat_map(fn i ->
         List.duplicate(1, i) |> Stream.concat([2])
       end)
     )
-    first_index = 6
+    first_index = 2
     cornerstone_index_stream = Stream.scan(index_deltas, first_index, &sum/2)
     cornerstone_index = Enum.at(cornerstone_index_stream, type_index)
     [cornerstone_index, cornerstone_index + 1, cornerstone_index + 2, box_number - 1]
@@ -89,68 +96,60 @@ defmodule Day3Part2 do
     |> indices
   end
 
-  # Orange indices:
-  # 6  , 9  , 12 , 16 , 20 , 25 , 30 , 36 , 42  , ...
-  # +3 , +3 , +4 , +4 , +5 , +5 , +6 , +6 , ...
+  def violets do
+    [{1, :violet, 0}]
+  end
+
+  def blues do
+    [{3, :blue, 0}, {5, :blue, 1}]
+  end
+
   def oranges do
-    deltas = Stream.concat([0], Stream.iterate(3, &add_1/1) |> Stream.flat_map(&dupe/1))
-    first_orange_index = 6
+    deltas = Stream.concat([0], Stream.iterate(2, &add_1/1) |> Stream.flat_map(&dupe/1))
+    first_orange_index = 2
     box_numbers = Stream.scan(deltas, first_orange_index, &sum/2)
 
-    type_indices = Stream.iterate(0, &add_1/1)
-
-    [box_numbers, Stream.cycle([:o]), type_indices]
-    |> Stream.zip
+    Stream.zip [box_numbers, Stream.cycle([:o]), index()]
   end
 
   # Yellow indices:
-  # 11 , 15 , 19 , 24 , 29 , 35 , 41  , ...
-  # +4 , +4 , +5 , +5 , +6 , +6 , ...
   def yellows do
-    deltas = Stream.concat([0], Stream.iterate(4, &add_1/1) |> Stream.flat_map(&dupe/1))
-    first_yellow_index = 11
+    deltas = Stream.concat([0, 3], Stream.iterate(4, &add_1/1) |> Stream.flat_map(&dupe/1))
+    first_yellow_index = 8
     box_numbers = Stream.scan(deltas, first_yellow_index, &sum/2)
 
-    type_indices = Stream.iterate(0, &add_1/1)
-
-    [box_numbers, Stream.cycle([:y]), type_indices]
-    |> Stream.zip
+    Stream.zip [box_numbers, Stream.cycle([:y]), index()]
   end
 
-  def cornerstone_greens do
+  def greens do
     deltas = Stream.concat(
       [0],
-      Stream.iterate(5, &add_1/1)
+      Stream.iterate(3, &add_1/1)
       |> Stream.flat_map(&dupe/1)
     )
-    first_cornerstone_green_index = 21
+    first_cornerstone_green_index = 7
     box_numbers = Stream.scan(deltas, first_cornerstone_green_index, &sum/2)
 
-    type_indices = Stream.iterate(0, &add_1/1)
-
-    [box_numbers, Stream.cycle([:cg]), type_indices]
-    |> Stream.zip
+    Stream.zip [box_numbers, Stream.cycle([:green]), index()]
   end
 
-  # Follower greens
-  # 22 , 23 , 27 , 28 , 32 , 33 , 34 , 38 , 39 , 40 , 44 , 45 , 46 , 47
-  # +1 , +4 , +1 , +4 , +1 , +1 , +4 , +1 , +1 , +4 , +1 , +1 , +1 , +4
-  def follower_greens do
+  def magentas do
     deltas = Stream.concat(
       [0],
-      Stream.iterate(1, &add_1/1)
+      Stream.iterate(0, &add_1/1)
       |> Stream.flat_map(&dupe/1)
       |> Stream.flat_map(fn i ->
         List.duplicate(1, i) |> Stream.concat([4])
       end)
     )
-    first_follower_green_index = 22
+    first_follower_green_index = 14
     box_numbers = Stream.scan(deltas, first_follower_green_index, &sum/2)
 
-    type_indices = Stream.iterate(0, &add_1/1)
+    Stream.zip [box_numbers, Stream.cycle([:magenta]), index()]
+  end
 
-    [box_numbers, Stream.cycle([:fg]), type_indices]
-    |> Stream.zip
+  def index do
+    Stream.iterate 0, &add_1/1
   end
 
   def add_1(n) do
