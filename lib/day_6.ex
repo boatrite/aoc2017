28d6c5fe93afa7bc332904e87d42b4c655c54d19
Gwen Boatrite <gwen.boatrite@gmail.com>
2017-12-11 18:57:32 -0600

    Day 6 pt 1

---
+++ lib/day_6.ex
@@ -0,0 +1,34 @@
+defmodule Day6 do
+
+  def solve(input) do
+    bank = String.split(input, ~r"\s")
+           |> Enum.map(&String.to_integer/1)
+    # feel the jank
+    Stream.iterate(2, &(&1 + 1))
+    |> Stream.transform({[bank], bank}, &transformer/2)
+    |> Enum.to_list
+    |> Enum.at(-1)
+  end
+
+  def transformer(i, {all_banks, curr_bank}) do
+    next_bank = do_cycle(curr_bank)
+    if Enum.member?(all_banks, next_bank) do
+      { :halt, :whatever }
+    else
+      next_all_banks = Enum.concat(all_banks, [next_bank])
+      { [i], {next_all_banks, next_bank} }
+    end
+  end
+
+  def do_cycle(bank) do
+    b_end = length(bank)
+    max = Enum.max(bank)
+    index = Enum.find_index(bank, &(&1 == max))
+    map = Enum.zip(0..b_end-1, bank) |> Map.new |> Map.put(index, 0)
+    Enum.reduce(1..max, map, fn i, map ->
+      x = index + i |> Integer.mod(b_end)
+      Map.put(map, x, Map.get(map, x)+1)
+    end)
+    |> Map.values
+  end
+end
---
+++ test/day_6_test.exs
@@ -0,0 +1,13 @@
+defmodule Day6Test do
+  use ExUnit.Case
+
+  test "solve" do
+    assert Day6.solve("0 2 7 0") == 5
+    assert Day6.solve("11	11	13	7	0	15	5	5	4	4	1	1	7	1	15	11") == 4074
+  end
+
+  test "do_cycle" do
+    assert Day6.do_cycle([0, 2, 7, 0]) == [2, 4, 1, 2]
+    assert Day6.do_cycle([2, 4, 1, 2]) == [3, 1, 2, 3]
+  end
+end
